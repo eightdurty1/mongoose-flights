3bf55575463ac50e8eab33bbc00bb32695e6345d
Israel Flores <israelflo92@gmail.com>
2022-04-05 01:47:19 -0700

    completed lab one

--- routes/flights.js
+++ routes/flights.js
@@ -6,6 +6,7 @@ router.get("/", flightsCtrl.index);
 router.get("/new", flightsCtrl.new);
 router.post('/', flightsCtrl.create);
 router.get('/:id', flightsCtrl.show);
+// router.post('/:id/destinations', flightsCtrl.)
 
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
